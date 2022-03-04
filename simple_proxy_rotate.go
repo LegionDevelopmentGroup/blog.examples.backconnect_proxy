44057c560851eaa2cb127e9b9478d849bee91ce1
Daniel Pasut <daniel.j.pasut@gmail.com>
2022-03-04 12:28:51 -0500

    Add code samples for backconnect proxy pt3.

---
+++ simple_proxy_rotate.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"github.com/elazarl/goproxy"
+	"log"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Rotater struct {
+	availableIPs []string
+	currentIndex int
+	m            *sync.Mutex
+}
+
+func (r *Rotater) nextIP() string {
+	r.m.Lock()
+	defer r.m.Unlock()
+	if r.currentIndex >= len(r.availableIPs) {
+		r.currentIndex = 0
+	}
+	n := r.availableIPs[r.currentIndex]
+	r.currentIndex += 1
+	return n
+}
+
+func (r *Rotater) CustomDialer(ctx context.Context, network string, addr string) (net.Conn, error) {
+	altIP := r.nextIP()
+	ipAddress := net.ParseIP(altIP)
+	d := net.Dialer{
+		LocalAddr: &net.TCPAddr{
+			IP: ipAddress,
+		},
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	return d.Dial(network, addr)
+}
+
+type TransportWrapper struct {
+	transport *http.Transport
+}
+
+func (t *TransportWrapper) RoundTrip(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Response, error) {
+	return t.transport.RoundTrip(req)
+}
+
+func WrapTransport(t *http.Transport) *TransportWrapper {
+	return &TransportWrapper{t}
+}
+
+func main() {
+	rotater := &Rotater{
+		availableIPs: []string{
+			"x.x.x.1",
+			"x.x.x.2",
+		},
+		m: &sync.Mutex{},
+	}
+
+	proxy := goproxy.NewProxyHttpServer()
+
+	// For debugging
+	proxy.Verbose = true
+
+	proxy.OnRequest().HandleConnectFunc(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+		// This handles incoming https Requests, (they use CONNECT) to forward the encrypted request
+		// For the basic version we reject with goproxy.RejectConnect
+		// To handle https proxy requests we'd return gorpoxy.MitmConnect
+
+		// Note there is a bug in elazarl/goproxy they fail to correctly close the connections (they use defer close in loops). This causes theye proxy (while mitm) where a connection may be re-used and therefore the IP used for a request is not rotated.
+		// It does generally work for the simple IP retrieval example, however should be addressed before use in production scraping.
+		return goproxy.MitmConnect, host
+	})
+
+// An alternative would be to reject all connect requests.	/*proxy.OnRequest().HandleConnectFunc(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+		return goproxy.RejectConnect, host
+	})*/
+
+	proxy.OnRequest().DoFunc(func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+
+		ctx.RoundTripper = WrapTransport(
+			&http.Transport{
+				Proxy:                 http.ProxyFromEnvironment,
+				DialContext:           rotater.CustomDialer,
+				MaxIdleConns:          1,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+				DisableKeepAlives:     true,
+			},
+		)
+
+		return r, nil
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", proxy))
+}
---
+++ testclient.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./testclient <target-site> <proxy-address>")
+		return
+	}
+
+	address := os.Args[1]
+	proxyAddress := os.Args[2]
+
+	proxyURL, err := url.Parse(proxyAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &http.Transport{
+			Proxy:           http.ProxyURL(proxyURL),
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("GET", address, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	start := time.Now()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Request Took: ", time.Now().Sub(start))
+
+	defer resp.Body.Close()
+
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%q\n", dump)
+}
